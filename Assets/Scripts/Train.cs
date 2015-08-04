392d48bb81074920a7bfe2a554e64d12834551c4
anastasia <arnika@1cv8.org.ua>
2015-08-04 14:51:57 +0300

    cделаны вагончики

---
+++ Assets/Scripts/Train.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Train : MonoBehaviour {
+	public float speed = 60F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = transform.position;
+		pos.z += speed * Time.deltaTime;
+		transform.position = pos;
+	
+	}
+}
---
+++ Assets/Scripts/wagon.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wagon : MonoBehaviour {
+	public float speed, max;
+	float minus = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = transform.position;
+
+		if (Mathf.Abs (pos.x) <= max) {
+			pos.x += speed * Time.deltaTime * minus;
+		} 
+		else 
+		{
+			pos.x = max * minus;
+			minus = minus * -1f;
+
+
+		}
+
+		transform.position = pos;
+	
+	}
+}
