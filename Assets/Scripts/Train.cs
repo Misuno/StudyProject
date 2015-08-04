eb8bf087e44fe6cb6aca607e02db978726d170a9
Misuno <alex@misuno.org>
2015-08-04 18:06:19 +0300

    Работа с вращением

--- Assets/Scripts/Train.cs
+++ Assets/Scripts/Train.cs
@@ -3,16 +3,72 @@ using System.Collections;
 
 public class Train : MonoBehaviour {
 	public float speed = 60F;
+	public float timeToMove = 4f;
+	public float turnAngle = 45f;
+	public float turnSpeed;
+
+	float neededTime;
+	Quaternion finalRotation;
+	Quaternion currentRotation;
 
 	// Use this for initialization
 	void Start () {
-	
+		neededTime = Time.time + timeToMove;
+		finalRotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f,turnAngle, 0f));
+		currentRotation = transform.rotation;
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {	
 		
 		transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+		// Проверяем, можно ли уже поворачивать.
+		if (Time.time >= neededTime)
+		{
+			// Находим кватернион нового вращения.
+			var rotation = Quaternion.RotateTowards(transform.rotation, finalRotation, turnSpeed * Time.deltaTime); 
+
+			// Если новое вращение не отличается от текущего, значит вращение завершено
+			if (transform.rotation == rotation)
+			{
+				// Назначаем новое целевое вращение.
+				finalRotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f,turnAngle, 0f));
+
+				// Заводим таймер на новое время.
+				neededTime = Time.time + timeToMove;
+			}
+			// Если вращение не завершено.
+			else 
+			{
+				// Применяем вращение.
+				transform.rotation = rotation;
+			}
+		}
+
+//		// Проверяем, можно ли уже поворачивать.
+//		if (Time.time >= neededTime)
+//		{
+//			// Рассчитываем коэффициент времени (от 0 до 1).
+//			float t = Mathf.Clamp01((Time.time - neededTime) / turnSpeed);
+//
+//			// Интерполируем текущее врещение от currrentRotation до finalRotation.
+//			transform.rotation = Quaternion.Lerp(currentRotation, finalRotation, t);
+//
+//			// Если вращение завершено (коэффициент стал == 1)
+//			if (t == 1)
+//			{
+//				// Заводим новый таймер.
+//				neededTime = Time.time + timeToMove;
+//
+//				// Выставляем новое значение начального вращения (текущее).
+//				currentRotation = transform.rotation;
+//
+//				// И финального вращения.
+//				finalRotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f,turnAngle, 0f));
+//			}
+//		}
 
 
 	
--- Assets/Scripts/wagon.cs
+++ Assets/Scripts/wagon.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class wagon : MonoBehaviour {
 	public float speed, max;
-	float minus = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,30 +11,6 @@ public class wagon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-//		var pos = transform.position;
-//
-//		if (Mathf.Abs (pos.x) <= max) {
-//			pos.x += speed * Time.deltaTime * minus;
-//		} 
-//		else 
-//		{
-//			pos.x = max * minus;
-//			minus = minus * -1f;
-//
-//
-//		}
-//
-//		transform.position = pos;
-
-		float rotation = speed * Time.deltaTime;
-
-		var angle = transform.rotation.z;
-		if (angle <= max) {
-//			rotation = 
-
-		}
-
-		transform.Rotate (0f, 0f, rotation);
-	
+	 	
 	}
 }
